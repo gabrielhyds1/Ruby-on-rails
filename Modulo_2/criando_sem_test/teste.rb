4abc5d92f25741be145d24b6db94fdf519cb5486
gabrielhyds1 <gabriel.yoshino@bradial.com.br>
2025-10-31 13:51:00 -0300

    aula 119 / 120

---
+++ Modulo_2/criando_sem_test/teste.rb
@@ -0,0 +1,18 @@
+# rails new testeapp -T
+
+# o -T é para não criar os arquivos de teste
+
+# Esse comando cria uma aplicação Rails sem os arquivos de teste padrão do Rails.
+
+# Depois de criar a aplicação, você pode navegar até o diretório da aplicação e iniciar o servidor Rails:
+
+# cd testeapp
+# rails server
+
+# Agora, você pode acessar a aplicação no seu navegador em http://localhost:3000.
+
+# Se você quiser adicionar testes mais tarde, pode configurar uma estrutura de teste de sua escolha, como RSpec ou Minitest, manualmente.
+
+# Comandos úteis para gerenciar o Spring (um pré-carregador de aplicativos Rails):
+# spring status
+# spring stop
