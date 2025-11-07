46fea0f3e27ae258bfed0c40a77b044f65c3a85b
gabrielhyds1 <gabriel.yoshino@bradial.com.br>
2025-11-07 15:15:39 -0300

    aula 130

--- Modulo_5/time_to_answer/app/controllers/welcome_controller.rb
+++
@@ -1,5 +0,0 @@
-class WelcomeController < ApplicationController
-  #http_basic_authenticate_with name: "admin", password:  "admin"
-  def index
-  end
-end
--- Modulo_5/time_to_answer/config/routes.rb
+++ Modulo_5/time_to_answer/config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  namespace :site do
+    get "welcome/index"
+  end
   namespace :profiles_backoffice do
     get "welcome/index"
   end
@@ -7,18 +10,17 @@ Rails.application.routes.draw do
   end
   devise_for :profiles
   devise_for :admins
-  get "welcome/index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
-  get "inicio" => "welcome#index", as: :inicio
+  get "inicio" => "site/welcome#index", as: :inicio
   # Render dynamic PWA files from app/views/pwa/*
   get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
   get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
 
   # Defines the root path route ("/")
   # root "posts#index"
-  root to: "welcome#index"
+  root to: "site/welcome#index"
 end
